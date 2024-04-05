e949cf650957dba426ee4796d2d15a9a51d2f853
Monish Agrawal <monish.21910923@viit.ac.in>
2024-04-05 21:20:12 +0530

    First commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Free Poll Maker
+      </h1>
+      <p  className="mt-5 text-4xl">
+        Create engaging live polls and get instant feedback in real time. Free & Unlimited.
+      </p>
+      
+    </>
+  )
+}
+
+export default App
